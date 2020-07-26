e313afa56aa8fa17f34b20e05218a8d2859302ec
= <=>
2020-07-26 19:26:44 +0100

    Fixed to use the correct method for clipboard clearing

--- src/electron/messages/clipboard.ts
+++ src/electron/messages/clipboard.ts
@@ -1,6 +1,5 @@
 import { IpcMainEvent, clipboard } from "electron";
 
 export const clearClipboard = (__: IpcMainEvent) => {
-    console.log("clearing");
-    clipboard.writeText("");
+    clipboard.clear();
 }
