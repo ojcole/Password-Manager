a6235a6e48a73f90b7f6ae7ff29e651c4d809425
Oliver Cole <oliver@ojcole.com>
2022-01-18 14:38:54 +0000

    Updated lower security password formula and redirected to ojcole from Hive7

--- src/generation/constants.ts
+++ src/generation/constants.ts
@@ -4,8 +4,6 @@ const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowercase = 'abcdefghijklmnopqrstuvwxyz';
 const numbers = '0123456789';
 const specialChars = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ ';
-const limitedSpecialChars = '!$@?.#';
 
 export const symbols = uppercase + lowercase + numbers + specialChars;
-export const limitedSymbols =
-  uppercase + lowercase + numbers + limitedSpecialChars;
+export const limitedSymbols = uppercase + lowercase + numbers;
